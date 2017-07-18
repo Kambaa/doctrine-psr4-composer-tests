7bcf40b345f0107982030c1d8a53654a5ae23e7e
Yusuf Gündüz <yusuf.gunduz@epias.com.tr>
2017-07-18 13:20:04 +0300

    doctrine orm create,update,executeSql stuff.

--- src/main/webapp/cli.php
+++ src/main/webapp/cli.php
@@ -6,6 +6,7 @@
  */
 
 
+use Doctrine\ORM\Tools\SchemaValidator;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
@@ -15,19 +16,36 @@ $paths = array(PHARDIR . "php/Entity");
 $isDevMode = false;
 
 // the connection configuration
+//$dbParams = array(
+//    'driver' => 'pdo_mysql',
+//    'host'=>'127.0.0.1',
+//    'user' => 'root',
+//    'password' => '1234565',
+//    'dbname' => 'doctrine_test',
+//);
 $dbParams = array(
     'driver' => 'pdo_mysql',
     'host'=>'127.0.0.1',
-    'user' => 'root',
-    'password' => '1234565',
+    'user' => 'doctrine_test',
+    'password' => 'doctrine_test',
     'dbname' => 'doctrine_test',
 );
 
+
+
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
 $entityManager = EntityManager::create($dbParams, $config);
-
+// USAGE : http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/tools.html
 $tool = new \Doctrine\ORM\Tools\SchemaTool($entityManager);
 $classes = array(
     $entityManager->getClassMetadata('AppSkeleton\Entity\User')
 );
-$tool->createSchema($classes);
+//$tool->createSchema($classes);
+$tool->updateSchema($classes);
+//var_dump($tool->getDropDatabaseSQL());
+var_dump($tool->getCreateSchemaSql($classes));
+
+
+# For import sql statements: http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/native-sql.html#native-sql
+//$dbh = $entityManager->getConnection();
+//$dbh->executeUpdate(...);
